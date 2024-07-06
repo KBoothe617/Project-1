05f7f9c94b444a967bb30a2c63d9d324ab2065b4
Ash <aishasiddiqa151@gmail.com>
2024-07-06 16:11:21 -0700

    add functions to JS

--- assets/script.js
+++ assets/script.js
@@ -11,9 +11,31 @@ function saveRecipes(recipes) {
 // Function to load DOM content
 document.addEventListener('DOMContentLoaded', function() {
 
+// Modal element will be declared here to get the modal
+
+// Declaring variable for the "Add New Recipe" button that opens the modal
+const addNewRecipeBtn = document.getElementById('addNewRecipe');
+
 // Function for modal/form pop up when "Add New Recipe" button is clicked
+addNewRecipeBtn.addEventListener('click', function() {
+    
+});
+
 
 // Function for form submission
+const form = document.getElementById('recipeForm');
+form.addEventListener('submit', function(event) {
+    event.preventDefault (); // Prevent default form submission
+
+// Retrieve values of form
+const title = document.getElementById('recipeTitle').value.trim();
+const directions = document.getElementById('directions').value.trim();
+const cookingTime = document.getElementById('cookingTime').value.trim();
+
+// Close modal
+
+})
+});
 
 // Function to display recipe on the sidebar
 
@@ -21,4 +43,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Function to sort recipes by cooking time
 
-});
