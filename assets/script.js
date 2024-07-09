bc63bac21a0245bcea22f282c1fba157b72196e8
Ash <aishasiddiqa151@gmail.com>
2024-07-08 20:15:16 -0700

    get modal, button that opens modal and close

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,18 @@
-// Function to get recipes from localStorage or return empty array
-function getRecipes() {
-    return JSON.parse(localStorage.getItem('recipes')) || [];
+// Declare modal variable and get the modal element by ID
+const modal = document.getElementById("addRecipeModal");
+
+// Declare btn variable and get the button element by ID that opens the modal
+const btn = document.getElementById("addNewRecipe");
+
+// Declare span variable and get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on "Add New Recipe" button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
 }
 
-// Function to save recipes to localStorage
-function saveRecipes(recipes) {
-    localStorage.setItem('recipes', JSON.stringify(recipes));
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
 }
-
-// Function to load DOM content
-document.addEventListener('DOMContentLoaded', function() {
-
-// Modal element will be declared here to get the modal
-
-// Declaring variable for the "Add New Recipe" button that opens the modal
-const addNewRecipeBtn = document.getElementById('addNewRecipe');
-
-// Function for modal/form pop up when "Add New Recipe" button is clicked
-addNewRecipeBtn.addEventListener('click', function() {
-    
-});
-
-
-// Function for form submission
-const form = document.getElementById('recipeForm');
-form.addEventListener('submit', function(event) {
-    event.preventDefault (); // Prevent default form submission
-
-// Retrieve values of form
-const title = document.getElementById('recipeTitle').value.trim();
-const directions = document.getElementById('directions').value.trim();
-const cookingTime = document.getElementById('cookingTime').value.trim();
-
-// Close modal
-
-})
-});
-
-// Function to display recipe on the sidebar
-
-// Function to add a delete button for each recipe
-
-// Function to sort recipes by cooking time
-
